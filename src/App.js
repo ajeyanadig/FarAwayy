e173275fc14d7bc46983808e1ba7b82c412fc82c
ajeyanadig <ajeyanadig@gmail.com>
2024-03-14 12:13:08 +0530

    change2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Stats from "./components/Stats";
+import List from "./components/List";
+import Logo from "./components/Logo";
 
-function App() {
+export default function App() {
+  const [travelItems, setTravelItems] = useState([]);
+
+  function addItemsHandler(newVal) {
+    setTravelItems([...travelItems, newVal]);
+  }
+  function deleteItemsHandler(id) {
+    setTravelItems(travelItems.filter((curr) => id !== curr.id));
+  }
+  function toggleItemStatus(id) {
+    setTravelItems(
+      travelItems.map((curr) =>
+        curr.id === id ? { ...curr, packed: !curr.packed } : curr
+      )
+    );
+  }
+  function deleteAllItemsHandler() {
+    setTravelItems([]);
+  }
+  //sort left
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form travelItems={travelItems} onAddItems={addItemsHandler} />
+      <List
+        travelItems={travelItems}
+        onDeleteItem={deleteItemsHandler}
+        onToggle={toggleItemStatus}
+        onClear={deleteAllItemsHandler}
+      />
+      <Stats travelItems={travelItems} />
     </div>
   );
 }
-
-export default App;
